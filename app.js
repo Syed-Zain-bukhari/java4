ac9b93635a67249d69f80119425623b7dd1e233e
Syed-Zain-bukhari <syedzain9398@gmail.com>
2025-02-26 13:28:25 +0500

    update app.js file

---
+++ app.js
@@ -0,0 +1,70 @@
+//Q 1
+var  studentNames = [];
+
+//Q 2
+var studentNames = {};
+
+//Q 3
+var fruits = ["Apple", "Banana", "Cherry"];
+
+//Q 4
+var num = [5, 10, 20];
+
+//Q 5
+var result = [true,false];
+
+//Q 6
+var total = ["user",true,15,"city"];
+
+//Q 7
+var education = ["SSC <br>" ,"HSC <br>", "BCS <br>","BS <br>", "BCOM <br>", "MS <br>",];
+document.write(education);
+
+//Q 8
+var students = ["Michael", "John", "Tony"];
+var score = [320, 230, 480];
+
+document.write("score of",students[0], "is",score[0],"percentage:",score[0]*100/500);
+document.write("score of",students[1], "is",score[1],"percentage:",score[1]*100/500);
+document.write("score of",students[2], "is",score[2],"percentage:",score[2]*100/500);
+
+//Q 10
+// var score = [320, 230, 480];
+// Arrays.sort(score);
+
+//Q 11
+
+var cities = ["karachi", "lahore", "peshawar", "quetta", "islamabad"];
+selectdCities = cities.slice(0,2);
+console.log(selectdCities);
+
+//Q 12
+
+var arr = ["This ", " is ", " my ", " cat"];
+var result = arr.join(""); 
+console.log(result);
+
+//Q13
+
+var stationary = [];
+
+
+stationary.push("keyboard");
+stationary.push("mouse");
+stationary.push("monitor");
+
+console.log(stationary);
+
+
+var removedItem1 = stationary.shift(); 
+var removedItem2 = stationary.shift(); 
+var removedItem3 = stationary.shift(); 
+console.log( removedItem1);
+console.log( removedItem2);
+console.log( removedItem3);
+
+
+
+
+
+
